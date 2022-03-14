0f612de3c7eb549c82a8c6be2364b90586f5aad6
hrackedz <85070360+hrackedz@users.noreply.github.com>
2022-03-14 20:14:24 +0100

    Update time-converter.py

--- time-converter.py
+++ time-converter.py
@@ -18,7 +18,6 @@ while data.lower() != 'quit':
     If you want to convert hours to days enter : htd
     If you want to convert seconds to days enter : std
     If you want to convert minutes to days enter : mtd
-    If you want to convert hours to days enter : htd
     If you want to convert seconds to hours enter : hts
     
     Type QUIT to quit the program!
@@ -28,27 +27,23 @@ while data.lower() != 'quit':
          dts = int(input('Please enter the number of days : '))
          res = int(dts * 24 * 60 * 60)
          print(dts, ' Days Converted to : ', res, 'seconds')
-    elif data.lower() == 'htd':
-         htd = input('Please enter the amount of Hours : ')
 
-         days = htd / 24
-         print(htd, 'Hours converted to days : ', days)
     elif data.lower() == 'std':
         std = int(input('Please enter the amount of seconds : '))
-        std1= std / (24 * 3600)
-        print(std , 'Seconds Converted to days : ' , "%.2f" % std1 , 'Days')
+        std1= float(std / (24 * 3600))
+        print(std , 'Seconds Converted to days : ' , "{:.2f}".format(std1) , 'Days')
     elif data.lower() == 'mtd':
         mtd = int(input('Please enter the amount of minutes : '))
-        mtd1 = mtd / (24 * 60)
-        print(mtd , 'Minutes converted to days : ' , "%.2f" % mtd1 , 'Days')
+        mtd1 = float(mtd / (24 * 60))
+        print(mtd , 'Minutes converted to days : ' , "{:.2f}".format(mtd1), 'Days')
     elif data.lower() == 'htd':
         htd = int(input('Please enter the amount of hours : '))
-        htd1 = htd / 24
-        print(htd , 'Hours converted to : ', "%.2f" % htd1 , 'Days')
+        htd1 = float(htd / 24)
+        print(htd , 'Hours converted to : ', "{:.2f}".format(htd1), 'Days')
     elif data.lower() == 'sth':
         sth = int(input('Please enter the amount of seconds : '))
-        sth1 = sth / 3600
-        print(sth, 'Seconds converted to : ', "%.2f" % sth1, 'Hours')
+        sth1 = float(sth / 3600)
+        print(sth, 'Seconds converted to : ', "{:.2f}".format(sth1) , 'Hours')
     elif data.lower() == quit :
         break
 
