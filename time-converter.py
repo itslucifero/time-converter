77a607d85f80d35cc1f1d49a3e768b52415c9b7a
Yahia Ouali <85070360+hrackedz@users.noreply.github.com>
2022-03-15 14:36:26 +0100

    Update time-converter.py

--- time-converter.py
+++ time-converter.py
@@ -22,19 +22,26 @@ while data.lower() != 'quit':
     Type QUIT to quit the program!
 
        ''')
+
+
+        #Converting days to seconds which  comes with minutes and hours
     elif data.lower() == 'dts':
-        dts = int(input('Please enter the number of days : '))
-        res = dts * 24 * 60 * 60
-        res1 = dts * 24 * 60
-        res2 = dts * 24
+        dts = int(input('Please enter the number of days : '))  #user input
+        res = dts * 24 * 60 * 60   # converting to seconds
+        res1 = dts * 24 * 60     #and here to minutes
+        res2 = dts * 24         #here to hours
+        #here the output
         print(dts, ' Days Converted to : ', str(res), ' seconds , Which is ' + str(res1) + ' minutes ,Which is ' + str(res2) + ' Hours')
 
+        #converting seconds to days
     elif data.lower() == 'std':
         std = int(input('Please enter the amount of seconds : '))
-        std1 = std // (24 * 3600)
-        std2 = (std - std1 * 3600 * 24) // 3600
-        std3 = (std - std1 * 3600 * 24 - std2 * 3600) // 60
+        std1 = std // (24 * 3600) #here seconds to days with floor division so we keep the remainder  of seconds away
+        std2 = (std - std1 * 3600 * 24) // 3600  # here we count the remainder from seconds to hours
+        std3 = (std - std1 * 3600 * 24 - std2 * 3600) // 60  #here we count the remainder from seconds and hours to minutes
         print(std, 'Seconds Converted to days : ', str(std1), 'Days And ' + str(std2) + ' Hours And ' + str(std3) + ' Minutes')
+
+
     elif data.lower() == 'mtd':
         mtd = int(input('Please enter the amount of minutes : '))
         mtd1 = mtd // (24 * 60)
@@ -51,6 +58,8 @@ while data.lower() != 'quit':
         sth1 = float(sth / 3600)
         sth2 = (sth - sth1 * 3600 ) // 60
         print(sth, 'Seconds converted to : ', str(sth1), 'Hours And ' + str(sth2) + ' Minutes')
+
+        #breaking the loop with quit
     elif data.lower() == quit:
         break
 
